e5b09b344b8bbff379b3cf91f40a774adc7e2442
ripon <riponahmed2201@gmail.com>
2023-11-11 15:37:19 +0600

    home page chane

--- resources/views/frontend/layouts/fourth_section.blade.php
+++ resources/views/frontend/layouts/fourth_section.blade.php
@@ -1,59 +1,14 @@
-{{-- <section class="mb-30 mt-30 wow fadeInUp">
-    <div class="container">
-
-        <div class="mx-3">
-            <div class="row">
-
-                <div class="col-md-6 d-flex justify-content-center align-items-center"
-                    style="min-height: 240px; border: 1px solid #ffffff; background-image: url('assets/frontend/images/fourth_section/2.jpg'); background-size: cover;">
-                    <div>
-                        <h2 class="text-white">Featured Content </h2>
-                    </div>
-                </div>
-
-                <div class="col-md-6 d-flex justify-content-center align-items-center"
-                    style="min-height: 240px;background-image: url('assets/frontend/images/fourth_section/1.jpg'); background-size: cover; border: 1px solid #ffffff">
-                    <a href="/practices-areas">
-                        <div>
-                            <h2 class="text-white">Practice Areas</h2>
-                        </div>
-                    </a>
-                </div>
-
-            </div>
-
-            <div class="row">
-                <div class="col-md-6 d-flex justify-content-center align-items-center"
-                    style="min-height: 240px;background-image: url('assets/frontend/images/fourth_section/4.png'); background-size: cover; border: 1px solid #ffffff">
-
-                    <a href="/international-recognition">
-                        <div>
-                            <h2 class="text-white">International Recognition</h2>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="col-md-6 d-flex justify-content-center align-items-center"
-                    style="min-height: 240px;background-image: url('assets/frontend/images/fourth_section/3.png'); background-size: cover; border: 1px solid #ffffff">
-
-                    <a href="/resources">
-                        <div>
-                            <h2 class="text-white">Resources</h2>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> --}}
-
-
+<style>
+    .custom_hover_color:hover {
+        color: #fff;
+    }
+</style>
 <section id="tabs-2" class="wide-60 tabs-section division">
     <div class="container">
         <div class="row row-cols-1 row-cols-md-2 d-flex align-items-center">
 
             <!-- IMAGE BLOCK -->
-            <div class="col">
+            <div class="col-md-6">
                 <div class="img-block right-column wow fadeInUp">
                     <div class="">
                         <h1 class="text-start">Find our Activities and Credentials</h1>
@@ -62,47 +17,41 @@
             </div>
 
             <!-- TABS NAVIGATION -->
-            <div class="col">
+            <div class="col-md-6">
                 <div class="tabs-nav clearfix">
                     <ul class="tabs-1">
 
-                        <!-- TAB-1 LINK -->
-                        <li class="tab-link current" data-tab="tab-1">
+                        <li class="tab-link">
+                            <a href="/practices-areas" class="custom_hover_color">
+                                <h5 class="h5-md">Practice Areas</h5>
 
-                            <!-- Title -->
-                            <h5 class="h5-md">Practice Areas</h5>
-
-                            <!-- Text -->
-                            <p class="p-sm">Semper lacus cursus porta feugiat primis a luctus ultrice tellus potenti
-                                neque dolor in primis
-                            </p>
-
+                                <p class="p-sm">Semper lacus cursus porta feugiat primis a luctus ultrice tellus
+                                    potenti
+                                    neque dolor in primis
+                                </p>
+                            </a>
                         </li>
 
-                        <!-- TAB-2 LINK -->
-                        <li class="tab-link" data-tab="tab-2">
+                        <li class="tab-link">
+                            <a href="/international-recognition" class="custom_hover_color">
+                                <h5 class="h5-md">International Recognition</h5>
 
-                            <!-- Title -->
-                            <h5 class="h5-md">International Recognition</h5>
-
-                            <!-- Text -->
-                            <p class="p-sm">Semper lacus cursus porta feugiat primis a luctus ultrice tellus potenti
-                                neque dolor in primis
-                            </p>
-
+                                <p class="p-sm">Semper lacus cursus porta feugiat primis a luctus ultrice tellus
+                                    potenti
+                                    neque dolor in primis
+                                </p>
+                            </a>
                         </li>
 
-                        <!-- TAB-3 LINK -->
-                        <li class="tab-link" data-tab="tab-3">
+                        <li class="tab-link">
+                            <a href="/resources" class="custom_hover_color">
+                                <h5 class="h5-md">Resources</h5>
 
-                            <!-- Title -->
-                            <h5 class="h5-md">Resources</h5>
-
-                            <!-- Text -->
-                            <p class="p-sm">Semper lacus cursus porta feugiat primis a luctus ultrice tellus potenti
-                                neque dolor in primis
-                            </p>
-
+                                <p class="p-sm">Semper lacus cursus porta feugiat primis a luctus ultrice tellus
+                                    potenti
+                                    neque dolor in primis
+                                </p>
+                            </a>
                         </li>
                     </ul>
                 </div>
