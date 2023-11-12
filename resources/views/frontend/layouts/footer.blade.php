ca95e03a7efa88f0e6d1968ea29554e37a113406
ripon <riponahmed2201@gmail.com>
2023-11-12 19:34:18 +0600

    home header and footer color change

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer-1" class="footer"
-    style="background-color: #002349; color:white; padding-top: 70px; padding-bottom: 0px;">
+    style="background-color: #023B59; color:white; padding-top: 70px; padding-bottom: 0px;">
     <div class="container">
 
         <!-- FOOTER CONTENT -->
