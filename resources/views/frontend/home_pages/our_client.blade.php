9c31d609b0e8dfbea55eb13a5f37831e831f78ba
ripon <riponahmed2201@gmail.com>
2023-11-16 11:28:08 +0600

    our client text change

--- resources/views/frontend/home_pages/our_client.blade.php
+++ resources/views/frontend/home_pages/our_client.blade.php
@@ -2,12 +2,17 @@
     <div class="container">
         <div class="row wow fadeInUp">
             <div class="col-md-4">
-                <h2 style="font-size: 40px; color: white; margin-right: 40px; font-weight: bold;">
+                {{-- <h2 style="color: white; margin-right: 40px; font-weight: bold;">
                     Our Client
-                </h2>
+                </h2> --}}
+
+                <div class="section-title">
+                    <h1  style="color: white;"> Our Client</h1>
+                </div>
+
             </div>
             <div class="col-md-8">
-                <p class="text-white p-sm" style="margin-top: 30px">
+                <p class="text-white p-sm">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi voluptatem nulla, tempore saepe,
                     eius doloribus autem, obcaecati reiciendis impedit nisi optio quisquam aut odit dolorum! Velit
                     impedit necessitatibus nihil consectetur. <a href="/our-client-details"
@@ -28,8 +33,8 @@
                         @foreach ($clientList as $value)
                             <div class="brand-logo" style="background-color: white; margin-left: 20px;">
                                 {{-- <a href="/our-client-details?client={{ $value->category_id }}"> --}}
-                                    <img style="width: 191px; height: 96px; object-fit:contain" class="img-fluid"
-                                        src="{{ asset('uploads/our_client/' . $value->logo) }}" alt="brand-logo" />
+                                <img style="width: 191px; height: 96px; object-fit:contain" class="img-fluid"
+                                    src="{{ asset('uploads/our_client/' . $value->logo) }}" alt="brand-logo" />
                                 {{-- </a> --}}
                             </div>
                         @endforeach
